5182c0830ddd1e4755bcec7eebcca54d5be01579
joshkokatnur <jkokatnur@gmail.com>
2024-04-25 15:59:39 -0400

    (fix hacken F-2024-1505) lock pragma version to 0.8.20

--- contracts/zones/TokenLockupPlansHandler.sol
+++ contracts/zones/TokenLockupPlansHandler.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity ^0.8.20;
+pragma solidity 0.8.20;
 
 import {ZoneInterface} from "seaport-types/src/interfaces/ZoneInterface.sol";
 import {ZoneParameters, Schema} from "seaport-types/src/lib/ConsiderationStructs.sol";
