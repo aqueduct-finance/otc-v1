d6a10b59e239d48874015cc65667cd3c8d3b01da
joshkokatnur <jkokatnur@gmail.com>
2024-07-22 16:05:48 -0400

    add bnb chain to hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -41,6 +41,11 @@ const config: HardhatUserConfig = {
       url: process.env.ARBITRUM_URL || "",
       accounts: [process.env.PRIVATE_KEY || ""],
     },
+    bnb: {
+      chainId: 56,
+      url: process.env.BNB_URL || "",
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
   },
   etherscan: {
     apiKey: {
@@ -49,6 +54,7 @@ const config: HardhatUserConfig = {
       polygon: process.env.POLYGONSCAN_API_KEY ?? "",
       base: process.env.BASESCAN_API_KEY ?? "",
       arbitrumOne: process.env.ARBITRUM_API_KEY ?? "",
+      bnb: process.env.BNB_API_KEY ?? "",
     },
   },
   solidity: "0.8.20",
